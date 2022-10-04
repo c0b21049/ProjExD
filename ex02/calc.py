df059192bae1b67826d98e53851f821abc2b85d8
Kabeya <c0b21049bd@edu.teu.ac.jp>
2022-10-04 14:28:58 +0900

    ２まで

---
+++ ex02/calc.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("300x500")
+r=0
+c=0
+for i, num in enumerate(range(9, -1, -1), 1):
+    button = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    button.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r+=1
+        c=0
+
+root.mainloop()
