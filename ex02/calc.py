cc03a5fa25b3e323f67ec1f6d0f6327ceda3a9b5
Kabeya <c0b21049bd@edu.teu.ac.jp>
2022-10-04 18:08:09 +0900

    コメントを追加しました #9

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,28 +1,28 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-import webbrowser
+import webbrowser #URLからサイトへ飛ぶためのwebbrowserをインポート
 
-url = "https://www.amazon.co.jp/"
+url = "https://www.amazon.co.jp/" #amazonのURL
 
-def click_num(event):
+def click_num(event): #数字を入力した場合
     btn = event.widget
     num = btn["text"]
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
 
-def click_equal(event):
+def click_equal(event): #=を入力した場合
     eq = entry.get()
     ans = eval(eq)
     entry.delete(0, tk.END)
     entry.insert(tk.END, ans)
 
-def click_Clear(event):
+def click_Clear(event): #Cを入力した場合
     entry.delete(0, tk.END)
 
-def click_amazon(event):
+def click_amazon(event): #aを入力した場合
     webbrowser.open(url)
 
-def click_percent(event):
+def click_percent(event): #%を入力した場合
     per = entry.get()
     ans = eval(f"{per}/100")
     entry.delete(0, tk.END)
@@ -41,7 +41,7 @@ BUTTON=[
     ["4", "5", "6", "-"],
     ["1", "2", "3", "*"],
     ["C", "0", "%", "/"]
-]
+]#ボタンを生成するための配置(リスト)
 for i in BUTTON:
     for j in range(len(i)):
         num = i[j]    
@@ -58,12 +58,12 @@ for i in BUTTON:
             r += 1
             c = 0
 
-button = tk.Button(root, text="a", font=("", 30), width=3, height=1, bg = "#ffa500")
+button = tk.Button(root, text="a", font=("", 30), width=3, height=1, bg = "#ffa500") #ボタンaを配置
 button.bind("<1>", click_amazon)
 button.grid(row=r, column=c)
 c = 3
 
-button = tk.Button(root, text="=", font=("", 30), width=3, height=1, bg = "#87cefa")
+button = tk.Button(root, text="=", font=("", 30), width=3, height=1, bg = "#87cefa") #ボタン=を配置
 button.bind("<1>", click_equal)
 button.grid(row=r, column=c)
 
