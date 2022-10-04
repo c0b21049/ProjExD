c683ae6481bcd620d4c59a94f82746b096c4abec
Kabeya <c0b21049bd@edu.teu.ac.jp>
2022-10-04 15:30:17 +0900

    ７まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,14 +3,25 @@ import tkinter.messagebox as tkm
 
 def click_num(event):
     btn = event.widget
-    num = int(btn["text"])
-    tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    num = btn["text"]
+    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+    entry.insert(tk.END, num)
+
+def click_equal(event):
+    eq = entry.get()
+    ans = eval(eq)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, ans)
 
 root = tk.Tk()
 root.geometry("300x500")
-r=0
+
+entry = tk.Entry(root, width=10, font=("", 40), justify="right")
+entry.grid(row=0, column=0, columnspan=3)
+
+r=1
 c=0
-for i, num in enumerate(range(9, -1, -1), 1):
+for i, num in enumerate(list(range(9, -1, -1))+["+"], 1):
     button = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     button.bind("<1>", click_num)
     button.grid(row=r, column=c)
@@ -18,5 +29,9 @@ for i, num in enumerate(range(9, -1, -1), 1):
     if i%3 == 0:
         r+=1
         c=0
+
+button = tk.Button(root, text="=", font=("", 30), width=4, height=2)
+button.bind("<1>", click_equal)
+button.grid(row=r, column=c)
 
 root.mainloop()
