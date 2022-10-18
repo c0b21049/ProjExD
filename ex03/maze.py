7461e3fcabec446a14551c493caa7d3903d7310f
Kabeya <c0b21049bd@edu.teu.ac.jp>
2022-10-18 14:22:15 +0900

    練習問題 03-3

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
     
+    tori = tk.PhotoImage(file="ex03/fig/5.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori")
     root.mainloop()
