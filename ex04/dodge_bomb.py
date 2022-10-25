e7e02d08e3213e184ca659ba04084bd2ed0acde7
Kabeya <c0b21049bd@edu.teu.ac.jp>
2022-10-25 14:39:37 +0900

    練習04-4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,16 +7,27 @@ def main():
     scrn_sfc = pg.display.set_mode((1600,900))
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
+
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
     
     clock = pg.time.Clock()
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)   
-        pg.display.update() 
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                break
-            
+                return
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP]:tori_rct.centery -=1#こうかとんの縦座標を-1
+        if key_states[pg.K_DOWN]:tori_rct.centery +=1#こうかとんの縦座標を+1
+        if key_states[pg.K_LEFT]:tori_rct.centerx -=1
+        if key_states[pg.K_RIGHT]:tori_rct.centerx +=1
+        scrn_sfc.blit(tori_sfc, tori_rct)    
+
+        pg.display.update() 
         clock.tick(1000)
 
 if  __name__ == "__main__":
